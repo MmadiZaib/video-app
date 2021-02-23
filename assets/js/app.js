0e58d81ff41860abf4b871d4906b04c8650823ca
Zaïb Mmmadi <zaib2010@gmail.com>
2021-02-23 14:02:47 +0100

    setup theme with webpack encore

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import '../css/app.css';
+import '../css/fontawesome.min.css';
+import '../css/styles.css';
+
+
+const $ = require('jquery');
+require('bootstrap');
+require('bootstrap/js/dist/tooltip');
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
