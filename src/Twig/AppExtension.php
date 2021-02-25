52da22fbffc562ec612c258fde3b564e4c280b7e
Zaïb Mmmadi <zaib2010@gmail.com>
2021-02-25 13:14:45 +0100

    create slugify twig filter

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('slugify', [AppRuntime::class, 'slugify']),
+        ];
+    }
+}
---
+++ src/Twig/AppRuntime.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Twig;
+
+
+use Twig\Extension\RuntimeExtensionInterface;
+
+class AppRuntime implements RuntimeExtensionInterface
+{
+    public function slugify(string $url): string
+    {
+        return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $url)));
+    }
+}
