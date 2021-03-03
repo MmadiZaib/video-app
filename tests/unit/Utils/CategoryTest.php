a350d2413adfde77aba6d73fbf05b3f4b8b30af0
Zaïb Mmmadi <zaib2010@gmail.com>
2021-03-03 19:03:49 +0100

    refactoring code and add new test

--- tests/unit/Utils/CategoryTest.php
+++ tests/unit/Utils/CategoryTest.php
@@ -11,17 +11,31 @@ class CategoryTest extends KernelTestCase
 
     protected $mockedCategoryTreeFrontPage;
 
+    protected $mockedCategoryTreeAdminList;
+
+    protected $mockedCategoryTreeAdminOptionList;
+
     protected function setUp(): void
     {
         $kernel = self::bootKernel();
         $urlGenerator  = $kernel->getContainer()->get('router');
 
-        $this->mockedCategoryTreeFrontPage = $this->getMockBuilder(CategoryTreeFrontPage::class)
-            ->disableOriginalConstructor()
-            ->setMethods()
-            ->getMock();
 
-        $this->mockedCategoryTreeFrontPage->urlGenerator = $urlGenerator;
+        $testedClasses = [
+            'CategoryTreeAdminList',
+            'CategoryTreeAdminOptionList',
+            'CategoryTreeFrontPage'
+
+        ];
+
+        foreach ($testedClasses as $class) {
+            $name = 'mocked' . $class;
+            $this->$name = $this->getMockBuilder('App\\Utils\\' . $class)
+                ->disableOriginalConstructor()
+                ->setMethods()
+                ->getMock();
+            $this->$name->urlGenerator = $urlGenerator;
+        }
 
     }
 
@@ -38,6 +52,26 @@ class CategoryTest extends KernelTestCase
         $this->assertSame($string, $this->mockedCategoryTreeFrontPage->getCategoryList($array));
     }
 
+    /** @dataProvider  dataForCategoryTreeAdminOptionList */
+    public function testCategoryTreeAdminOptionList(array $arrayToCompare, $arrayFromDb): void
+    {
+        $this->mockedCategoryTreeAdminOptionList->categoriesArray = $arrayFromDb;
+        $array= $this->mockedCategoryTreeAdminOptionList->buildTree();
+
+        $this->assertSame($arrayToCompare, $this->mockedCategoryTreeAdminOptionList->getCategoryList($array));
+    }
+
+    /** @dataProvider dataForCategoryTreeAdminList */
+    public function testCategoryTreeAdminList(string $string, array $arrayFromDb): void
+    {
+        $this->mockedCategoryTreeAdminList->categoriesArray = $arrayFromDb;
+        $array = $this->mockedCategoryTreeAdminList->buildTree();
+
+        $this->assertSame($string, $this->mockedCategoryTreeAdminList->getCategoryList($array));
+    }
+
+
+
     public function dataForCategoryTreeFrontPage(): iterable
     {
         yield [
@@ -49,6 +83,35 @@ class CategoryTest extends KernelTestCase
                 ["id" => 28, "parent_id" => 27, "name" => "hpp"]
             ],
             1
+        ];
+    }
+
+    public function dataForCategoryTreeAdminOptionList(): iterable
+    {
+        yield [
+            [
+                ["name" => "Electro", "id" => 25],
+                ["name" => "--comp", "id" => 26],
+                ["name" => "----Lap", "id" => 27],
+                ["name" => "------hpp", "id" => 28],
+            ],
+            [
+                ["id" => 25, "parent_id" => null, "name" => "Electro"],
+                ["id" => 26, "parent_id" => 25, "name" => "comp"],
+                ["id" => 27, "parent_id" => 26, "name" => "Lap"],
+                ["id" => 28, "parent_id" => 27, "name" => "hpp"]
+            ],
+        ];
+    }
+
+
+    public function dataForCategoryTreeAdminList(): iterable
+    {
+        yield [
+          '<ul class="fa-ul text-left"><li><i class="fa-li fa fa-arrow-right"></i> Electronics<a href="/admin/edit-category/1">Edit</a><a onclick="return confirm(\'Are you sure?\');" href="/admin/delete-category/1">Delete</a></li></ul>',
+          [
+              ['id' => 1, 'parent_id' => null, 'name' => 'Electronics']
+          ]
         ];
     }
 }
