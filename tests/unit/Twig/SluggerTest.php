c0a2d26c86ed33e15d26dd94d642a4306c31671c
Zaïb Mmmadi <zaib2010@gmail.com>
2021-03-03 15:31:51 +0100

    testing slugify extension

---
+++ tests/unit/Twig/SluggerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\unit\Twig;
+
+use App\Twig\AppRuntime;
+use PHPUnit\Framework\TestCase;
+
+class SluggerTest extends TestCase
+{
+    /**
+     * @dataProvider getSlugs
+     */
+    public function testSlugify(string $string, string $slug): void
+    {
+        $slugger = new AppRuntime;
+
+        $this->assertSame($slug,$slugger->slugify($string));
+    }
+
+    public function getSlugs(): iterable
+    {
+        yield ['Cell phones', 'cell-phones'];
+        yield ['Lorem ipsum', 'lorem-ipsum'];
+        yield ['Remove Space', 'remove-space'];
+    }
+}
